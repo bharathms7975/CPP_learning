afbdaeb986d537bf1ded424052055010dc22902c
Bharath M S <149494033+bharathms7975@users.noreply.github.com>
2024-10-30 20:13:55 +0530

    Normalize line endings

--- L74 - Heap/implementation.cpp
+++ L74 - Heap/implementation.cpp
@@ -79,11 +79,11 @@ void heapify(int arr[], int n, int i)
     int left = 2 * i;
     int right = 2 * i + 1;
 
-    if (left < n && arr[largest] < arr[left])
+    if (left <= n && arr[largest] < arr[left])
     {
         largest = left;
     }
-    if (right < n && arr[largest] < arr[right])
+    if (right <= n && arr[largest] < arr[right])
     {
         largest = right;
     }
@@ -92,6 +92,19 @@ void heapify(int arr[], int n, int i)
     {
         swap(arr[i], arr[largest]);
         heapify(arr, n, largest);
+    }
+}
+
+void heapsort(int arr[], int n)
+{
+    int size = n;
+
+    while (size > 1)
+    {
+        swap(arr[size], arr[1]);
+        size--;
+
+        heapify(arr, size, 1);
     }
 }
 
@@ -120,6 +133,16 @@ int main()
         heapify(arr, 5, i);
     }
     cout << "Printing new heap:" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // heap sort driver code
+
+    heapsort(arr, 5);
+    cout << "Printing sorted heap:" << endl;
     for (int i = 1; i <= n; i++)
     {
         cout << arr[i] << " ";
